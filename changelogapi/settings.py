42ccfe3974f83bc5aed3d5506f25e90bb8df0f22
Jupalaja <jupalaja@gmail.com>
2023-01-03 17:41:05 -0500

    added settings

--- changelogapi/settings.py
+++ changelogapi/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRETdjango-insecure-wv4*e=%l%b@*epy$yy)h5743cv8cb1l0$!kggf(kut)r_r28ud'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
