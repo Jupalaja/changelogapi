828a4dab7368959cda2264cdc6d7b41ae04a4c5c
Jupalaja <jupalaja@gmail.com>
2023-01-04 12:01:04 -0500

    up to date

--- changelogapi/settings.py
+++ changelogapi/settings.py
@@ -110,7 +110,6 @@ REST_FRAMEWORK = {
         'authentication.jwt.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
